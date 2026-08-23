3e8ea188defbcb18113ec8b530f9729be931bea7
mirbek10 <m51869956@gmail.com>
2026-08-23 23:51:15 +0600

    save

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useQueryClient } from '@tanstack/react-query';
 import { AttendanceDashboard } from '../components/AttendanceDashboard';
-import { clearApiUrl } from '../api/client';
 
 export function DashboardPage() {
   const navigate = useNavigate();
